cf5e58e1202b1ad220e587039e9e57a47f83051b
r09943170 <r09943170@ntu.edu.tw>
2024-05-22 15:16:01 +0800

    move include to CHIP_All.sv

--- CHIP_All.sv
+++ CHIP_All.sv
@@ -5,6 +5,59 @@
 // ---------------------------
 // Li-Yang Huang <lyhuang@media.ee.ntu.edu.tw>, 2023
 
+`include "RgbdVoConfigPk.sv"
+`include "CHIP_fea.sv"
+`include "SRAM_all.sv"
+`include "FAST.sv"      //CHIP_fea.sv
+`include "BRIEF_Top.sv" //CHIP_fea.sv
+`include "MATCH_Top.sv" //CHIP_fea.sv
+`include "SMOOTH.sv"        //FAST.sv
+`include "FAST_9.sv"        //FAST.sv
+`include "NMS.sv"           //FAST.sv
+`include "Orientation.sv"   //FAST.sv
+// `include "DW_sqrt.v"         //Orientation.sv
+// `include "DW_div.v"          //Orientation.sv
+`include "Key_Buffer1.sv"   //BRIEF_Top.sv
+`include "BRIEF.sv"         //BRIEF_Top.sv
+`include "LUT.sv"               //BRIEF.sv
+`include "Key_Buffer2.sv"   //MATCH_Top.sv
+`include "MATCH.sv"         //MATCH_Top.sv
+`include "MATCH_mem.sv"     //MATCH_Top.sv
+`include "HAMMING.sv"           //MATCH.sv
+`include "sram_v3/sram_lb_FAST.v"
+`include "sram_v3/sram_dp_dstFrame.v"
+`include "sram_v3/sram_dp_desc_8.v"
+`include "./DW02_mult_2_stage.v"
+`include "./DW_div.v"
+`include "./DW_div_pipe.v"
+`include "./DW02_mult.v"
+`include "./DW_mult_pipe.v"
+`include "./DW_sqrt.v"
+`include "./DW_sqrt_pipe.v" //Rodrigues.sv"
+`include "./DW_sincos.v"    //Rodrigues.sv"
+`include "./Idx2Cloud.sv"
+`include "./TransMat.sv"
+`include "./Proj.sv"
+`include "./DataDelay.sv"
+`include "./IndirectCoe.sv"
+`include "./IndirectCalc.sv"
+`include "./MulAcc.sv"
+`include "./Matrix.sv"
+`include "./LDLT.sv"
+`include "./Solver.sv"
+`include "./Rodrigues.sv"
+`include "./UpdatePose.sv"
+`include "./ComputeCorresps.sv"
+`include "./normalComputer.sv"
+`include "./normalUnitization.sv"
+`include "./OuterProduct.sv"
+`include "./CalcICPLsmMatrices.sv"
+`include "./CalcRgbdLsmMatrices.sv"
+`include "./sigma_icp_generator.sv"
+`include "./sigma_rgbd_generator.sv"
+`include "./AtA_AtB_of_Direct.sv"
+`include "./gradient.sv"
+
 module CHIP_All
     import RgbdVoConfigPk::*;
 #(
